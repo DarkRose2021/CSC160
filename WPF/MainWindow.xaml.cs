2a3190d0bc421e421339cc0f14a2233de47dc6bf
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2022-08-03 09:50:01 -0600

    Continued with WPF

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -14,5 +14,18 @@ namespace WPF
 			//overrides the window's default title
 			Window.Title = "Neumont";
 		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show(sender.ToString());
+		}
+
+		private void FavFood_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+			//ComboBox cb = (ComboBoxd)sender;
+			//ComboBoxItem cbi = (ComboBoxItem)cb.SelectedItem;
+			//MessageBox.Show(cbi.SelecetedValue.ToString());
+			MessageBox.Show(cbi.Content.ToString());
+		}
 	}
 }
