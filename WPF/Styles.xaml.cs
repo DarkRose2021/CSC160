e8329e8df15386d2fefdc81bace88be40194dce3
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2022-08-08 09:33:44 -0600

    Did styling in WPF

---
+++ WPF/Styles.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WPF
+{
+	/// <summary>
+	/// Interaction logic for Styles.xaml
+	/// </summary>
+	public partial class Styles : Window
+	{
+		public Styles()
+		{
+			InitializeComponent();
+		}
+	}
+}
