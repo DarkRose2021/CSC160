82761bcdbea6dff28366b3bb0c3eaff1e63810d2
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2022-08-17 09:50:14 -0600

    worked with sql

---
+++ WPF/DB.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace WPF
+{
+	/// <summary>
+	/// Interaction logic for DB.xaml
+	/// </summary>
+	public partial class DB : Window
+	{
+		public DB()
+		{
+			InitializeComponent();
+		}
+
+		private void btnDoit_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
