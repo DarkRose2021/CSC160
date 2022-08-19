8f556fd66357cc759a97bfbb07539e757cd29483
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2022-08-19 09:59:48 -0600

    worked with sql

--- WPF/DB.xaml.cs
+++ WPF/DB.xaml.cs
@@ -9,6 +9,8 @@ namespace WPF
 	/// </summary>
 	public partial class DB : Window
 	{
+		Names3 ds = new Names3();
+		WPF.Names3TableAdapters.namesTableAdapter ad = new WPF.Names3TableAdapters.namesTableAdapter();
 		public DB()
 		{
 			InitializeComponent();
@@ -18,12 +20,35 @@ namespace WPF
 		{
 			DataTable dt;
 			Hashtable ht = new Hashtable();
+			DataRow dr;
 			string sql;
 			long lngReturn;
-			sql = "select * from names where ID=@ID";
+			int intID;
+			sql = "select ID from Users where Username=@username and password=@Password";
 			ht.Add("@ID", 3);
 			dt = ExDB.GetDataTable("AwesomeDB", ht, sql);
-			dg.ItemsSource = dt.DefaultView;
+			//dg.ItemsSource = dt.DefaultView;
+
+			if (dt.Rows.Count > 0)
+			{
+				dr = dt.Rows[0];
+				intID = (int)dr["ID"];
+				lblMsg.Content = intID;
+			}
+
+			ht.Clear();
+			sql = "Insert into names (Name, Age) values('Grace', 19)";
+			//lngReturn = ExDB.ExecuteIt("AwesomeDB", sql, ht);
+			//lblMsg.Content = lngReturn + " records affected";
+
+			//ad.Insert("Katie", 19);
+			ad.Fill(ds.names);
+			lblMsg.Content = ds.names.Rows.Count;
+
+			var s = (from names in ds.names
+					 where names.ID == 3
+					 select names.Name);
+			lblMsg.Content = s.First().ToString();
 		}
 	}
 }
