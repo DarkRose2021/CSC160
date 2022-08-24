e20e7cdc090ab0aee3383850b90ad2cf4079bef2
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2022-08-24 09:57:41 -0600

    worked on login

--- Login/MainWindow.xaml.cs
+++ Login/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections;
+using System.Windows;
 
 namespace Login
 {
@@ -14,7 +15,28 @@ namespace Login
 
 		private void btnSubmit_Click(object sender, RoutedEventArgs e)
 		{
+			lblmsg.Content = "";
+			Hashtable ht = new Hashtable();
+			string sql;
+			long lngreturn;
+			ht.Add("@Name", txtName.Text);
+			ht.Add("@Username", txtUsername.Text);
+			ht.Add("@Password", txtPass.Text);
+			ht.Add("@Email", txtEmail.Text);
 
+			sql = "select Username, Password from Users where Username = @Username and Password = @Password";
+			lngreturn = ExDB.ExecuteIt("AwesomeDB", sql, ht);
+
+			if (lngreturn != -1)
+			{
+				lblmsg.Content = "You've logged in";
+			}
+			else
+			{
+				sql = "insert into Users (Name, Username, Password, Email) values (@Name, @Username, @Password, @Email)";
+				lngreturn = ExDB.ExecuteIt("AwesomeDB", sql, ht);
+				lblmsg.Content = "Account Created";
+			}
 		}
 	}
 }
